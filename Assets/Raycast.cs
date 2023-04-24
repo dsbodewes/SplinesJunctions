80f8ff5dce2fed1084188ec49eb13d5b0f799b72
Diëgo <diegobodewes29@gmail.com>
2023-04-24 14:51:57 +0200

    Nieuwe Scene
    Splines en Railway setup

--- Assets/Raycast.cs
+++ Assets/Raycast.cs
@@ -11,22 +11,22 @@ public class Raycast : MonoBehaviour
         RaycastHit hit;
         bool Stop = false;
 
-        if ((Physics.Raycast(new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), transform.TransformDirection(Vector3.forward), out hit, 10)) && hit.transform.tag == "Car")
+        if ((Physics.Raycast(new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), transform.TransformDirection(Vector3.forward), out hit, 15)) && hit.transform.tag == "Car")
         {
             Debug.DrawRay(new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
             Stop = true;
         }
 
-        else if ((Physics.Raycast(new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), transform.TransformDirection(new Vector3(2, 0, 1)), out hit, 10)) && hit.transform.tag == "Car")
+        /*else if ((Physics.Raycast(new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), transform.TransformDirection(new Vector3(2, 0, 1)), out hit, 10)) && hit.transform.tag == "Car")
         {
             Debug.DrawRay(new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), transform.TransformDirection(new Vector3(2, 0, 1)) * hit.distance, Color.red);
             Stop = true;
-        }
+        }*/
 
         else
         {
-            Debug.DrawRay(new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), transform.TransformDirection(new Vector3(2, 0, 1)) * 8, Color.green);
-            Debug.DrawRay(new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), transform.TransformDirection(Vector3.forward) * 8, Color.green);
+            /*Debug.DrawRay(new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), transform.TransformDirection(new Vector3(2, 0, 1)) * 8, Color.green);*/
+            Debug.DrawRay(new Vector3(transform.position.x, transform.position.y + 2, transform.position.z), transform.TransformDirection(Vector3.forward) * 15, Color.green);
             Stop = false;
         }
 
